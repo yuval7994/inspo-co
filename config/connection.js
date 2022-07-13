e4841a79481e3703904a3adafec85180e4389a19
yuval7994 <yuvalba45@gmail.com>
2022-07-13 13:21:48 -0500

    added server.js, connection.js, and dotenv

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/inspo-co", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected 🌍"))
+
+mongoose.set("debug", true)
+
+module.exports = mongoose.connection
