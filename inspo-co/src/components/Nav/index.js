6deb888aec5e397fee655ba5ed4f3557611f1c63
Emily Arnholt <emilynarnholt@gmail.com>
2022-07-13 20:32:51 -0500

    updated index.js

---
+++ inspo-co/src/components/Nav/index.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+
+function Nav() {
+  return (
+
+    <div>
+      <header class ="header">
+        <h3>
+            Inspo.co
+        </h3>
+        <nav>
+            <button> Dashboard</button>
+           <button> Log In</button>
+            <button> Sign Up</button>
+        </nav>
+      </header>
+
+      {/* <div class="sign-in-button">
+        <body>
+            <h1>
+                SIGN UP
+            </h1>
+            <h5>
+                Email
+            </h5>
+            <h5>
+              Password
+            </h5>
+        </body>
+       
+      </div> */}
+
+    </div>
+   
+  );
+}
+
+export default Nav;
