331ccb5ec61e9ab9a0ee74ff4131db7f681d1147
HoneyTwix <chocolateenchanted@gmail.com>
2022-07-24 13:07:48 -0500

    heroku

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require("apollo-server-express")
-const { User, Photo } = require("../../models")
+const { User, Photo } = require("../models")
 const { signToken } = require("../utils/auth")
 
 const resolvers = {
